9faf1a34315e8e67437f4b8223d77057b1573ee3
Harry Evans <henryjohnevans@icloud.com>
2021-09-03 14:24:03 +0100

    heroku deployment

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { getToken } from './auth'
-
+import { baseUrl } from '../config'
 
 function headers() {
   return {
@@ -11,64 +11,64 @@ function headers() {
 // * STUDIO REQUESTS
 
 export function getAllStudios() {
-  return axios.get('/api/studios')
+  return axios.get(`${baseUrl}/studios`)
 }
 
 export function getSingleStudio(studioId) {
-  return axios.get(`/api/studios/${studioId}`, headers())
+  return axios.get(`${baseUrl}/studios/${studioId}`, headers())
 }
 
 export function createStudio(formData) {
-  return axios.post('/api/studios', formData, headers())
+  return axios.post(`${baseUrl}/studios`, formData, headers())
 }
 
 export function editStudio(studioId, formData) {
-  return axios.put(`/api/studios/${studioId}`, formData, headers())
+  return axios.put(`${baseUrl}/studios/${studioId}`, formData, headers())
 }
 
 export function deleteStudio(studioId) {
-  return axios.delete(`/api/studios/${studioId}`, headers())
+  return axios.delete(`${baseUrl}/studios/${studioId}`, headers())
 }
 
 
 // * COMMENT REQUESTS
 
 export function createComment(studioId, formData) {
-  return axios.post(`/api/studios/${studioId}/comments`, formData, headers())
+  return axios.post(`${baseUrl}/studios/${studioId}/comments`, formData, headers())
 }
 
 export function deleteComment(studioId, commentId) {
-  return axios.delete(`/api/studios/${studioId}/comments/${commentId}`, headers())
+  return axios.delete(`${baseUrl}/studios/${studioId}/comments/${commentId}`, headers())
 }
 
 // * BOOKING REQUESTS
 
 export function createBooking(studioId, formData) {
-  return axios.post(`/api/studios/${studioId}/bookings`, formData, headers())
+  return axios.post(`${baseUrl}/studios/${studioId}/bookings`, formData, headers())
 }
 
 export function deleteBooking(studioId, bookingId) {
-  return axios.delete(`/api/studios/${studioId}/bookings/${bookingId}`, headers())
+  return axios.delete(`${baseUrl}/studios/${studioId}/bookings/${bookingId}`, headers())
 }
 
 // * USER REQUESTS
 
 export function registerUser(formData) {
-  return axios.post('/api/register', formData)
+  return axios.post(`${baseUrl}/register`, formData)
 }
 
 export function loginUser(formData) {
-  return axios.post('/api/login', formData)
+  return axios.post(`${baseUrl}/login`, formData)
 }
 
 export function studioFavourited(studioId) {
-  return axios.post(`/api/studios/${studioId}/favourites`, headers(), headers())
+  return axios.post(`${baseUrl}/studios/${studioId}/favourites`, headers(), headers())
 }
 
 export function profileUser() {
-  return axios.get('/api/profile', headers())
+  return axios.get(`${baseUrl}/profile`, headers())
 }
 
 export function editUser(userId, formData) {
-  return axios.put(`/api/profile/${userId}`, formData, headers())
+  return axios.put(`${baseUrl}/profile/${userId}`, formData, headers())
 }
